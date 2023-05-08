7aebd46a0e7adb47626a49894a2a817d6e69ce22
Tatiana Korzyukova <korzioukova@gmail.com>
2023-05-08 13:50:21 -0400

    handles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
-    <div className="todoapp">
-     <h1>Todo app</h1>
+    <div className="todo-app">
+     <TodoForm />
     </div>
   );
 }
